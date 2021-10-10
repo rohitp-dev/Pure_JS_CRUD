3faca0d080e83e3f5285185ef0d470fc322485c2
Rohit <rohitpahuja149.rp@gmail.com>
2021-10-10 19:18:43 +0530

    crud

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+var selectedRow = null;
+
 function onFormsubmit() {
   var formData = readFormData();
-  insertNewRecord(formData);
+  if (selectedRow == null) {
+    insertNewRecord(formData);
+  } else {
+    updateRecord(formData);
+  }
+  resetForm();
 }
 
 function readFormData() {
@@ -15,7 +22,7 @@ function readFormData() {
 function insertNewRecord(data) {
   var table = document
     .getElementById("employeeList")
-    .getElementsByTagName('tbody')[0];
+    .getElementsByTagName("tbody")[0];
   var newRow = table.insertRow(table.length);
   cell1 = newRow.insertCell(0);
   cell1.innerHTML = data.fullname;
@@ -25,6 +32,38 @@ function insertNewRecord(data) {
   cell3.innerHTML = data.salary;
   cell4 = newRow.insertCell(3);
   cell4.innerHTML = data.city;
-  cell4.innerHTML = `<a>Edit</a>
-                    <a>Delete</a>`;
+  cell4 = newRow.insertCell(4);
+  cell4.innerHTML = `<a onclick="onEdit(this)">Edit</a>
+                    <a onclick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+  document.getElementById("fullname").value = "";
+  document.getElementById("empcode").value = "";
+  document.getElementById("salary").value = "";
+  document.getElementById("city").value = "";
+  selectRow = null;
+}
+
+function onEdit(td) {
+  selectRow = td.parentElement.parentElement;
+  document.getElementById("fullname").value = selectRow.cells[0].innerHTML;
+  document.getElementById("empcode").value = selectRow.cells[1].innerHTML;
+  document.getElementById("salary").value = selectRow.cells[2].innerHTML;
+  document.getElementById("city").value = selectRow.cells[3].innerHTML;
+}
+
+function updateRecord(formData) {
+  selectRow.cells[0].innerHTML = formData.fullname;
+  selectRow.cells[1].innerHTML = formData.empcode;
+  selectRow.cells[2].innerHTML = formData.salary;
+  selectRow.cells[3].innerHTML = formData.city;
+}
+
+function onDelete(td) {
+  if (confirm("Are you sure you want to delete ? ")) {
+    row = td.parentElement.parentElement;
+    document.getElementById("employeeList").deleteRow(row.rowIndex);
+    resetForm();
+  }
 }
